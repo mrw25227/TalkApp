7464c5435123a589ddcecf32d4251ce082da3345
HAO WANG <Andy_Wang>
2025-02-12 19:58:55 +0800

    update UI, try TTS and SST api

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -1,8 +1,12 @@
+using TextSpeech;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Android;
 
 public class MainScript : MonoBehaviour
 {
+    const string LANG_CODE = "cmn-Hant-TW";
+
     [SerializeField]
     InputField userInput;
     [SerializeField]
@@ -11,19 +15,44 @@ public class MainScript : MonoBehaviour
     [SerializeField]
     Button textInputBtn;
     [SerializeField]
-    Button speechInputBtn;
+    ClickHandler speechInputBtn;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         textInputBtn.onClick.AddListener(OnTextInputBtnClick);
-        speechInputBtn.onClick.AddListener(OnSpeechInputBtnClick);
+        speechInputBtn.downEvent.AddListener(StartUserSpeaking);
+        speechInputBtn.upEvent.AddListener(StopUserSpeaking);
+        InitSpeechSystem(LANG_CODE);
     }
 
     private void OnDestroy()
     {
         textInputBtn.onClick.RemoveAllListeners();
-        speechInputBtn.onClick.RemoveAllListeners();
+        speechInputBtn.downEvent.RemoveAllListeners();
+        speechInputBtn.upEvent.RemoveAllListeners();
+    }
+
+    void InitSpeechSystem(string code)
+    {
+        TextToSpeech.Instance.Setting(code, 1, 1);
+        SpeechToText.Instance.Setting(code);
+        
+        SpeechToText.Instance.onResultCallback = OnUserFinalSpeachResult;
+        SpeechToText.Instance.onErrorCallback = OnUserSpeachError;
+        SpeechToText.Instance.onPartialResultsCallback = OnUserPartialSpeechResult;
+
+        TextToSpeech.Instance.onStartCallBack = OnAISpeakStart;
+        TextToSpeech.Instance.onDoneCallback = OnAISpeakStop;
+        CheckPermission();
+    }
+
+    void CheckPermission()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
     }
 
     // Update is called once per frame
@@ -35,11 +64,57 @@ public class MainScript : MonoBehaviour
     void OnTextInputBtnClick()
     {
         string inputText = userInput.text;
+        StartAISpeaking(inputText);
     }
 
-    void OnSpeechInputBtnClick()
+
+    public void StartAISpeaking(string message)
+    {
+        TextToSpeech.Instance.StartSpeak(message);
+    }
+
+    public void StopAISpeacking()
+    {
+        TextToSpeech.Instance.StopSpeak();
+    }
+
+    void OnAISpeakStart()
     {
 
+    }
+
+    void OnAISpeakStop()
+    {
+
+    }
+
+    public void StartUserSpeaking()
+    {
+        SpeechToText.Instance.StartRecording();
+    }
+
+    public void StopUserSpeaking()
+    {
+        SpeechToText.Instance.StopRecording();
+    }
+
+    void OnUserFinalSpeachResult(string result)
+    {
+        userInput.text = result;
+        Debug.LogError("OnUserFinalSpeachResult");
+        StartAISpeaking(result);
+    }
+    void OnUserSpeachError(string result)
+    {
+        userInput.text = result;
+        Debug.LogError("OnUserSpeachError");
+        //StartAISpeaking(result);
+    }
+
+    void OnUserPartialSpeechResult(string result)
+    {
+        Debug.LogError("OnUserPartialSpeechResult: " + result);
+        userInput.text = result;
     }
 
 }

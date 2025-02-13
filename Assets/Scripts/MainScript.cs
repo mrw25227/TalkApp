cdf6fd1bfccca9566c066b6e2602cdcfae9e5d22
HAO WANG <Andy_Wang>
2025-02-13 17:20:34 +0800

    update user speech btn function, add chatGpt api make app can talk with user

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -2,6 +2,8 @@ using TextSpeech;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Android;
+using UnityEngine.Networking;
+using System.Collections;
 
 public class MainScript : MonoBehaviour
 {
@@ -15,27 +17,32 @@ public class MainScript : MonoBehaviour
     [SerializeField]
     Button textInputBtn;
     [SerializeField]
-    ClickHandler speechInputBtn;
+    Button speechInputBtn;
+
+    string modelName = "gpt-4o-mini";
+    const string prompt = "你是一個普通進行對話的AI，只能使用自然語言與使用者對話，請避免提供程式碼、特殊符號或無法朗讀的內容。";
+    private string apiKey = "";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        textInputBtn.onClick.AddListener(OnTextInputBtnClick);
-        speechInputBtn.downEvent.AddListener(StartUserSpeaking);
-        speechInputBtn.upEvent.AddListener(StopUserSpeaking);
-        InitSpeechSystem(LANG_CODE);
-    }
+        Input.backButtonLeavesApp = true;
 
+        textInputBtn.onClick.AddListener(OnTextInputBtnClick);
+        speechInputBtn.onClick.AddListener(StartUserSpeaking);
+        InitSpeechSystem(LANG_CODE);
+        TextAsset textAsset = Resources.Load<TextAsset>("api_key");
+        apiKey = textAsset.text;
+    }
     private void OnDestroy()
     {
         textInputBtn.onClick.RemoveAllListeners();
-        speechInputBtn.downEvent.RemoveAllListeners();
-        speechInputBtn.upEvent.RemoveAllListeners();
+        speechInputBtn.onClick.RemoveAllListeners();
     }
 
     void InitSpeechSystem(string code)
     {
-        TextToSpeech.Instance.Setting(code, 1, 1);
+        TextToSpeech.Instance.Setting(code, 1, 1.5f);
         SpeechToText.Instance.Setting(code);
         
         SpeechToText.Instance.onResultCallback = OnUserFinalSpeachResult;
@@ -55,18 +62,20 @@ public class MainScript : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTextInputBtnClick()
     {
         string inputText = userInput.text;
-        StartAISpeaking(inputText);
+        if(inputText.Length == 0)
+        {
+            StartAISpeaking("你說什麼，我沒聽到?");
+            return;
+        }
+        StartCoroutine(GetAIResponse(inputText, modelName));
+        
     }
 
+    
 
     public void StartAISpeaking(string message)
     {
@@ -101,20 +110,77 @@ public class MainScript : MonoBehaviour
     void OnUserFinalSpeachResult(string result)
     {
         userInput.text = result;
-        Debug.LogError("OnUserFinalSpeachResult");
-        StartAISpeaking(result);
+        OnTextInputBtnClick();
     }
     void OnUserSpeachError(string result)
     {
         userInput.text = result;
-        Debug.LogError("OnUserSpeachError");
-        //StartAISpeaking(result);
+        Debug.LogError("OnUserSpeachError: " + result);
     }
 
     void OnUserPartialSpeechResult(string result)
     {
-        Debug.LogError("OnUserPartialSpeechResult: " + result);
         userInput.text = result;
     }
 
+    IEnumerator GetAIResponse(string message, string model)
+    {
+        AIInput.text = "思考中...";
+        string apiUrl = "https://api.openai.com/v1/chat/completions";
+        string requestMessage = prompt + " " + message;
+        string requestBody = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + requestMessage.Trim() + "\"}],\"max_tokens\": 500}";
+
+        using (UnityWebRequest request = new UnityWebRequest(apiUrl, "POST"))
+        {
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(requestBody);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Authorization", "Bearer " + apiKey);
+
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                string responseText = request.downloadHandler.text;
+                OpenAIResponse response = JsonUtility.FromJson<OpenAIResponse>(responseText);
+                Debug.Log("RESONSE: " + responseText);
+                AIInput.text = response.choices[0].message.content;
+#if UNITY_ANDROID && !UNITY_EDITOR
+                StartAISpeaking(AIInput.text);
+#endif
+            }
+            else
+            {
+                Debug.LogError("Error: " + request.error);
+                if(request.error.Contains("Too Many Requests"))
+                {
+                    AIInput.text = "已經超過對話額度，請稍後再試！";
+                }
+                else
+                {
+                    AIInput.text = "AI 無法回應，請稍後再試！";
+                }
+                
+            }
+        }
+    }
+    [System.Serializable]
+    private class OpenAIResponse
+    {
+        public Choice[] choices;
+    }
+
+    [System.Serializable]
+    private class Choice
+    {
+        public Message message;
+    }
+
+    [System.Serializable]
+    private class Message
+    {
+        public string content;
+    }
+    
 }
